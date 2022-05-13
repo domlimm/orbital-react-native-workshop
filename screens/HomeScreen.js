8bbd4ed06ead74e4f19b78cf168bd150e283fe4d
domlimm <dominiclimkj3448@gmail.com>
2022-05-13 15:56:23 +0800

    Add navigation buttons and styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View, Pressable, Text, ToastAndroid } from 'react-native';
 import React from 'react';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+    const showToast = () => {
+        ToastAndroid.show('Coming soon...!', ToastAndroid.SHORT);
+    };
+
     return (
-        <View>
-            <Button title='Test' />
+        <View style={styles.container}>
+            <Pressable
+                onPress={() => navigation.navigate('Ex1Incorrect')}
+                style={styles.button}
+                android_ripple={{ color: 'FFF' }}
+            >
+                <Text style={styles.text}>Exercise 1 (Incorrect)</Text>
+            </Pressable>
+            <Pressable
+                onPress={() => navigation.navigate('Ex1Solution')}
+                style={styles.button}
+                android_ripple={{ color: 'FFF' }}
+            >
+                <Text style={styles.text}>Exercise 1 (Solution)</Text>
+            </Pressable>
+            <Pressable
+                style={styles.button}
+                android_ripple={{ color: 'FFF' }}
+                onPress={showToast}
+            >
+                <Text style={styles.text}>21 May Workshop</Text>
+            </Pressable>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        backgroundColor: '#407BFF',
+        marginVertical: 10,
+        paddingVertical: 10,
+        width: '80%',
+        alignItems: 'center',
+        borderRadius: 4
+    },
+    text: {
+        color: 'white'
+    }
+});
 
 export default HomeScreen;
