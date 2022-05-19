d0f91f019191eafc7f556d4a86664b6c133c372c
domlimm <dominiclimkj3448@gmail.com>
2022-05-20 01:23:41 +0800

    Add logout functionality

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { auth } from '../firebase';
 import {
@@ -17,56 +19,6 @@ import {
 
 const Stack = createNativeStackNavigator();
 const TodoStack = createNativeStackNavigator();
-
-const MainNavigator = () => (
-    <Stack.Navigator initialRouteName='Main'>
-        <Stack.Screen
-            name='Main'
-            options={{ title: 'Orbital 22 React Native Workshop' }}
-            component={MainScreen}
-        />
-        <Stack.Screen
-            name='Ex1Incorrect'
-            options={{ title: 'Exercise 1 (Incorrect)' }}
-            component={Ex1Incorrect}
-        />
-        <Stack.Screen
-            name='Ex1Solution'
-            options={{ title: 'Exercise 1 (Solution)' }}
-            component={Ex1Solution}
-        />
-        <Stack.Screen
-            name='Ex1SolutionRedux'
-            options={{ title: 'Exercise 1 (Solution Redux)' }}
-            component={Ex1SolutionRedux}
-        />
-        <Stack.Screen
-            name='Ex2Solution'
-            options={{ title: 'Exercise 2 (Solution)' }}
-            component={Ex2Solution}
-        />
-        <Stack.Screen
-            name='CountRedux'
-            options={{ title: 'Count with specific input' }}
-            component={CountRedux}
-        />
-        <Stack.Screen
-            name='Auth'
-            options={{ headerTitle: 'TodoList' }}
-            component={AuthScreen}
-        />
-    </Stack.Navigator>
-);
-
-const TodoNavigator = () => (
-    <TodoStack.Navigator>
-        <TodoStack.Screen
-            name='Home'
-            options={{ headerTitle: 'Home' }}
-            component={HomeScreen}
-        />
-    </TodoStack.Navigator>
-);
 
 const AppNavigator = () => {
     /**
@@ -96,6 +48,72 @@ const AppNavigator = () => {
         // app stops running.
         return unsubscribeAuthStateChanged;
     }, []);
+
+    const MainNavigator = () => (
+        <Stack.Navigator initialRouteName='Main'>
+            <Stack.Screen
+                name='Main'
+                options={{ title: 'Orbital 22 React Native Workshop' }}
+                component={MainScreen}
+            />
+            <Stack.Screen
+                name='Ex1Incorrect'
+                options={{ title: 'Exercise 1 (Incorrect)' }}
+                component={Ex1Incorrect}
+            />
+            <Stack.Screen
+                name='Ex1Solution'
+                options={{ title: 'Exercise 1 (Solution)' }}
+                component={Ex1Solution}
+            />
+            <Stack.Screen
+                name='Ex1SolutionRedux'
+                options={{ title: 'Exercise 1 (Solution Redux)' }}
+                component={Ex1SolutionRedux}
+            />
+            <Stack.Screen
+                name='Ex2Solution'
+                options={{ title: 'Exercise 2 (Solution)' }}
+                component={Ex2Solution}
+            />
+            <Stack.Screen
+                name='CountRedux'
+                options={{ title: 'Count with specific input' }}
+                component={CountRedux}
+            />
+            <Stack.Screen
+                name='Auth'
+                options={{ headerTitle: 'TodoList' }}
+                component={AuthScreen}
+            />
+        </Stack.Navigator>
+    );
+
+    const logoutHandler = () => {
+        signOut(auth).then(() => {
+            setIsAuth(false);
+            setUser({});
+        });
+    };
+
+    const LogoutIcon = () => (
+        <TouchableOpacity onPress={logoutHandler}>
+            <MaterialIcons name='logout' size={28} color='#407BFF' />
+        </TouchableOpacity>
+    );
+
+    const TodoNavigator = () => (
+        <TodoStack.Navigator>
+            <TodoStack.Screen
+                name='Home'
+                options={{
+                    headerTitle: 'Home',
+                    headerRight: () => <LogoutIcon />
+                }}
+                component={HomeScreen}
+            />
+        </TodoStack.Navigator>
+    );
 
     return (
         <NavigationContainer>
