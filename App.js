63fc49f769abca44307842c081a679732ed15e4f
domlimm <dominiclimkj3448@gmail.com>
2022-05-19 22:52:57 +0800

    Refactor project structure

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
+import { LogBox } from 'react-native';
 
 import AppNavigator from './navigation/AppNavigator';
 import { store } from './store/store';
+
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 export default App = () => {
     return (
