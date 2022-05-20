627eaa9bd5a330cff426ee1230b40ae4a36cd699
domlimm <dominiclimkj3448@gmail.com>
2022-05-20 21:04:49 +0800

    Move flatlist dummy data into a separate file

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,6 +13,7 @@ import {
 import React, { useState } from 'react';
 
 import { Task } from '../components';
+import { data } from '../constants/dummy-data';
 
 const INPUT_PLACEHOLDER = 'Enter your task and hit the Add';
 const THEME = '#407BFF';
@@ -25,13 +26,6 @@ const HomeScreen = () => {
     const onSubmitHandler = () => {
         console.log('Adding task...');
     };
-
-    const data = [
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    ];
 
     return (
         <KeyboardAvoidingView
